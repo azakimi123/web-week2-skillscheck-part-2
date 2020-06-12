1d8db229662c3c3b8d835dbfd7b4ffbadb35e633
azakimi123 <aaron.zakimi@gmail.com>
2020-06-12 14:40:13 -0600

    finished (help understanding reference vs. value)

--- js-competencies/arrays-3/arrays-3.js
+++ js-competencies/arrays-3/arrays-3.js
@@ -10,8 +10,12 @@
 
 var sunsetColors = ["brown", "purple", "pink", "red", "orange", "yellow"]
 
-//Code here
-
+// //Code here
+function sunset(arr) {
+    let copyArr = sunsetColors;
+    copyArr.splice(0,1,"blue");
+    return copyArr;
+}
 
 //////////////////PROBLEM 2////////////////////
 
@@ -23,7 +27,11 @@ var sunriseColors = ["yellow", "orange", "red", "pink", "purple", "blue"]
 
 //Code here
 
-
+function sunrise(arr) {
+    let copyArr = sunriseColors.slice();
+    copyArr.splice(0,1,'green');
+    return copyArr;
+}
 //////////////////PROBLEM 3////////////////////
 
 //What is the difference between pass by reference, and pass by value?
@@ -41,11 +49,11 @@ var sunriseColors = ["yellow", "orange", "red", "pink", "purple", "blue"]
 //                 the copy and the original; a copy made by value will only 
 //                 change the copy's values`
 
-// var refVsVal = `Passing by reference points to the same object in memory;
-//                 pass by value creates a new object in memory with the same 
-//                 values. A change made to a reference copy will change both 
-//                 the copy and the original; a copy made by value will only 
-//                 change the copy's values`
+var refVsVal = `Passing by reference points to the same object in memory;
+                pass by value creates a new object in memory with the same 
+                values. A change made to a reference copy will change both 
+                the copy and the original; a copy made by value will only 
+                change the copy's values`
 
 // var refVsVal = `Passing by reference creates a new array in memory that 
 //                 points to the original array in memory; pass by value creates 
